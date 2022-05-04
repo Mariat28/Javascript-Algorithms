3e95f828836d8e29f394aab8b32b290e68d4c1b4
Mariat28 <mariatndagire28@gmail.com>
2022-05-04 11:40:38 +0300

    binary gap algorithm

--- BinaryGap.js
+++ BinaryGap.js
@@ -1,25 +1,36 @@
-// Write a function, function binaryGap(number);that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+// Find longest sequence of zeros in binary representation of an integer.
+
+// Task description
+// A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+
+// For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+
+// Write a function:
+
+// function solution(N);
+
+// that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
 // For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..2,147,483,647].
 function solution(number) {
   // write your code in JavaScript (Node.js 8.9.4)
     const binaryValue = number.toString(2);
-let gapCounter =0;
-let length = 0;
-let prevLength;
-let resultArray=[];
-  for(let x=0; x<=binaryValue.length; x++){
-    if(binaryValue[x] == '0'){
-      length++;
-      if(binaryValue[x+1] == '1'){
-        prevLength = length
-        console.log(length, prevLength);
-        gapCounter+=1;
-      // console.log(length);
-        resultArray.push(length);
+    let gapsArray = [];
+    let zeroCounter = 0;
+    for(number of binaryValue) {
+      if(number === '0'){
+        zeroCounter++;
+      } else{
+        gapsArray.push(zeroCounter);
+        zeroCounter = 0;
       }
     }
-  }
-  console.log(resultArray);
+    console.log(Math.max(...gapsArray));
 }
 
-solution(1041);
+solution(12456);
+
